7730d13ea5ff18906d333175b9a0a94ca1acfcc9
turtle <qqaz50313@gmail.com>
2021-07-27 16:28:36 +0800

    fix: Replace PUT with PATCH in cors config

--- server/config/cors.go
+++ server/config/cors.go
@@ -7,7 +7,7 @@ import (
 func CorsConfig()  cors.Config{
 	corsConf := cors.DefaultConfig()
 	corsConf.AllowAllOrigins = true
-	corsConf.AllowMethods = []string{"GET", "POST", "DELETE", "OPTIONS", "PUT"}
+	corsConf.AllowMethods = []string{"GET", "POST", "DELETE", "OPTIONS", "PATCH"}
 	corsConf.AllowHeaders = []string{"Authorization", "Content-Type", "Upgrade", "Origin",
 			"Connection", "Accept-Encoding", "Accept-Language", "Host", "Access-Control-Request-Method", "Access-Control-Request-Headers"}
 	return corsConf
